aa25f1a6144239e9817a0170dce0ccd9e1c9a7e8
Bodo Schulz <bodo@boone-schulz.de>
2021-10-07 08:07:53 +0200

    updates

--- filter_plugins/certificates.py
+++ filter_plugins/certificates.py
@@ -18,29 +18,98 @@ class FilterModule(object):
     def filters(self):
         return {
             'check_certificates': self.certificates,
+            'active_vhost': self.active_vhost,
+            'letsencrypt': self.letsencrypt,
+            'create_vhost': self.create_vhost,
         }
 
-    def certificates(self, data = None, stats = None):
+    def certificates(self, data=None, stats=None):
         """
         """
-        result = {}
-
-        display.v(" = data {} ({})".format(json.dumps(data, indent = 2), type(data)))
-
-        #display.v(" -> data  {} ({})".format(data, type(data)))
-        #display.v(" -> stats {} ({})".format(stats, type(stats)))
-
         r = stats.get('results', [])
-
-        display.v("  - {}".format(r))
         for k in r:
-            item = k.get('item', {}).get('key','')
-            stat = k.get('stat', {})
+            item = k.get('item', {}).get('key', '')
+            ssl = k.get('item', {}).get('value', {}).get('ssl', False)
+            lec = k.get('item', {}).get('value', {}).get('letsencrypt', False)
 
-            #display.v(" --[{}]--------------------------------------------".format(item))
-            if stat and not len(stat) == 0:
-                data[item]['ssl_certificate_exists'] = True
-
-        display.v(" = data {} ({})".format(json.dumps(data, indent = 2), type(data)))
+            if ssl or lec:
+                data[item]['ssl_certificate_exists'] = k.get('stat', {}).get('exists', False)
 
         return data
+
+    def active_vhost(self, data=None, certificate_exists=False):
+        """
+        """
+        display.v(" = data {} ({})".format(json.dumps(data, indent=2), type(data)))
+
+        result = False
+        enabled = True
+        present = True
+
+        value = data.get('value', {})
+
+        ssl = value.get("ssl", False)
+        letsencrypt = value.get("letsencrypt", False)
+
+        display.v(" -> enabled            {} ({})".format(enabled, type(enabled)))
+        display.v(" -> present            {} ({})".format(present, type(present)))
+        display.v(" -> ssl                {} ({})".format(ssl, type(ssl)))
+        display.v(" -> letsencrypt        {} ({})".format(letsencrypt, type(letsencrypt)))
+        display.v(" -> certificate_exists {} ({})".format(certificate_exists, type(certificate_exists)))
+
+        if enabled and present:
+            result = True
+
+            if not ssl and certificate_exists:
+                result = False
+
+            if letsencrypt:
+                result = False
+
+        display.v(" = result {}".format(result))
+
+        return result
+
+    def letsencrypt(self, data):
+        """
+        """
+        display.v(" = data {} ({})".format(json.dumps(data, indent=2), type(data)))
+
+        result = {}
+
+        for k, v in data.items():
+            display.v(" = data {} ({})".format(json.dumps(v, indent=2), type(v)))
+            if v.get('letsencrypt', False):
+                result[k] = dict(
+                    domains=v.get('domains', []),
+                    email=v.get('letsencrypt_email', '')
+                )
+
+        return result
+
+    def create_vhost(self, data, mode='http'):
+        """
+        """
+        display.v(" = data {} ({})".format(json.dumps(data, indent=2), type(data)))
+
+        result = False
+        present = True
+
+        value = data.get('value', {})
+
+        state = value.get("state", True)
+        ssl = value.get("ssl", False)
+        letsencrypt = value.get("letsencrypt", False)
+
+        display.v(" -> present            {} ({})".format(present, type(present)))
+        display.v(" -> ssl                {} ({})".format(ssl, type(ssl)))
+        display.v(" -> letsencrypt        {} ({})".format(letsencrypt, type(letsencrypt)))
+
+        if mode == 'http' and (present and not ssl and not letsencrypt):
+            result = True
+        if mode == 'https' and (present and ssl and not letsencrypt):
+            result = True
+        if mode == 'acme' and (present and ssl and letsencrypt):
+            result = True
+
+        return result
