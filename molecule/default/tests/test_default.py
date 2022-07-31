a13067aa2f7a6cbf510e65e5daf56a1e27797b9f
Bodo Schulz <bodo@boone-schulz.de>
2022-07-31 15:57:42 +0200

    add archlinux support

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,18 +1,31 @@
 
+from __future__ import unicode_literals
+
 from ansible.parsing.dataloader import DataLoader
 from ansible.template import Templar
+
+import json
 import pytest
 import os
+
 import testinfra.utils.ansible_runner
 
-import pprint
-pp = pprint.PrettyPrinter()
+HOST = 'instance'
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts(HOST)
+
+
+def pp_json(json_thing, sort=True, indents=2):
+    if type(json_thing) is str:
+        print(json.dumps(json.loads(json_thing), sort_keys=sort, indent=indents))
+    else:
+        print(json.dumps(json_thing, sort_keys=sort, indent=indents))
+    return None
 
 
 def base_directory():
+    """ ... """
     cwd = os.getcwd()
 
     if('group_vars' in os.listdir(cwd)):
@@ -25,24 +38,61 @@ def base_directory():
     return directory, molecule_directory
 
 
+def read_ansible_yaml(file_name, role_name):
+    ext_arr = ["yml", "yaml"]
+
+    read_file = None
+
+    for e in ext_arr:
+        test_file = "{}.{}".format(file_name, e)
+        if os.path.isfile(test_file):
+            read_file = test_file
+            break
+
+    return "file={} name={}".format(read_file, role_name)
+
+
 @pytest.fixture()
 def get_vars(host):
     """
-
+        parse ansible variables
+        - defaults/main.yml
+        - vars/main.yml
+        - vars/${DISTRIBUTION}.yaml
+        - molecule/${MOLECULE_SCENARIO_NAME}/group_vars/all/vars.yml
     """
     base_dir, molecule_dir = base_directory()
+    distribution = host.system_info.distribution
 
-    file_defaults = "file={}/defaults/main.yml name=role_defaults".format(base_dir)
-    file_vars = "file={}/vars/main.yml name=role_vars".format(base_dir)
-    file_molecule = "file={}/group_vars/all/vars.yml name=test_vars".format(molecule_dir)
+    print(" -> {}".format(distribution))
+    print(" -> {}".format(base_dir))
 
-    defaults_vars = host.ansible("include_vars", file_defaults).get("ansible_facts").get("role_defaults")
-    vars_vars = host.ansible("include_vars", file_vars).get("ansible_facts").get("role_vars")
-    molecule_vars = host.ansible("include_vars", file_molecule).get("ansible_facts").get("test_vars")
+    if distribution in ['debian', 'ubuntu']:
+        os = "debian"
+    elif distribution in ['redhat', 'ol', 'centos', 'rocky', 'almalinux']:
+        os = "redhat"
+    elif distribution in ['arch']:
+        os = "archlinux"
+
+    print(" -> {} / {}".format(distribution, os))
+
+    file_defaults      = read_ansible_yaml("{}/defaults/main".format(base_dir), "role_defaults")
+    file_vars          = read_ansible_yaml("{}/vars/main".format(base_dir), "role_vars")
+    file_distibution   = read_ansible_yaml("{}/vars/{}".format(base_dir, os), "role_distibution")
+    file_molecule      = read_ansible_yaml("{}/group_vars/all/vars".format(molecule_dir), "test_vars")
+    # file_host_molecule = read_ansible_yaml("{}/host_vars/{}/vars".format(base_dir, HOST), "host_vars")
+
+    defaults_vars      = host.ansible("include_vars", file_defaults).get("ansible_facts").get("role_defaults")
+    vars_vars          = host.ansible("include_vars", file_vars).get("ansible_facts").get("role_vars")
+    distibution_vars   = host.ansible("include_vars", file_distibution).get("ansible_facts").get("role_distibution")
+    molecule_vars      = host.ansible("include_vars", file_molecule).get("ansible_facts").get("test_vars")
+    # host_vars          = host.ansible("include_vars", file_host_molecule).get("ansible_facts").get("host_vars")
 
     ansible_vars = defaults_vars
     ansible_vars.update(vars_vars)
+    ansible_vars.update(distibution_vars)
     ansible_vars.update(molecule_vars)
+    # ansible_vars.update(host_vars)
 
     templar = Templar(loader=DataLoader(), variables=ansible_vars)
     result = templar.template(ansible_vars, fail_on_undefined=False)
@@ -52,10 +102,31 @@ def get_vars(host):
 
 def test_installed_package(host):
     p = host.package("nginx")
-    assert not p.is_installed
+    assert p.is_installed
 
+
+@pytest.mark.parametrize("dirs", [
+    "/etc/nginx/sites-available",
+    "/etc/nginx/sites-enabled",
+    "/etc/nginx/includes.d",
+    "/etc/nginx/conf.d",
+])
+def test_directories(host, dirs):
+    d = host.file(dirs)
+    assert d.is_directory
+
+
+@pytest.mark.parametrize("files", [
+    "/etc/nginx/nginx.conf",
+    "/etc/nginx/includes.d/nginx_log.conf",
+    "/etc/nginx/conf.d/ssl.conf",
+    "/etc/nginx/conf.d/gzip.conf",
+])
+def test_directories(host, files):
+    d = host.file(files)
+    assert d.is_file
 
 def test_service(host):
     service = host.service("nginx")
-    assert not service.is_enabled
-    assert not service.is_running
+    assert service.is_enabled
+    assert service.is_running
